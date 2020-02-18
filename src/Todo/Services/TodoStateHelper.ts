b0ae09573671d752cdda99944cf17281d305c110
rocket-libs <55183002+rocket-libs@users.noreply.github.com>
2020-02-18 11:52:40 +0300

    Changed the helper from a class to just functions.
    Helps extend the scope of the shuttleFx variable

--- src/Todo/Services/TodoStateHelper.ts
+++ src/Todo/Services/TodoStateHelper.ts
@@ -3,27 +3,22 @@ import TodoRespository from "../Shuttler/TodoRespository";
 import TodoState from "../Shuttler/TodoState";
 import TodoItem from "../Data/TodoItem";
 
+let shuttlerFx: ShuttlerFx<TodoRespository,TodoState,TodoItem>;
+export default function initializeHelper(shuttlerFxProp: ShuttlerFx<TodoRespository,TodoState,TodoItem>){
+        shuttlerFx = shuttlerFxProp;
+        shuttlerFx.state.isValidTodo = this.isValidTodo;
+}
 
 
-export default class TodoStateHelper{
-    shuttlerFx: ShuttlerFx<TodoRespository,TodoState,TodoItem>
-    constructor(shuttlerFx: ShuttlerFx<TodoRespository,TodoState,TodoItem>){
-        this.shuttlerFx = shuttlerFx;
-
-        this.shuttlerFx.state.isValidTodo = this.isValidTodo;
-    }
-
-
-    isValidTodo () : boolean {
-        if(!this.shuttlerFx.target){
-            return false;
+const isValidTodo =  () : boolean => {
+    if(!shuttlerFx.target){
+        return false;
+    }else{
+        if(shuttlerFx.target.displayLabel && shuttlerFx.target.priority){
+            return true;
         }else{
-            if(this.shuttlerFx.target.displayLabel && this.shuttlerFx.target.priority){
-                return true;
-            }else{
-                return false;
-            }
+            return false;
         }
     }
-
 }
+
